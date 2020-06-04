a81d1f9417022afbfc22a158de046b18e38dfcd6
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-06-04 16:43:10 -0300

    changed obterEmprestimo method

--- src/Cliente.java
+++ src/Cliente.java
@@ -49,7 +49,18 @@ public class Cliente extends Pessoa implements InterfaceCliente{
 	public boolean obterEmprestimo(int valor) {
 		
 		//TODO implemente seu codigo aqui
+
+		if(valor <= 0) return false;
+
+		int dividaTotal = valor + valorDaDivida;
 		
+		if(dividaTotal > dividaMaxima) {
+			return false;
+		} else {
+			valorDaDivida = dividaTotal;
+			valorContaCorrente = valor + valorContaCorrente;
+		}
+
 		return false;
 	}
 	
